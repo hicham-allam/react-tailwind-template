50bea4d2cb9d55d09f31204c6631c07d27d95b1f
hicham-allam <hichamallam02@gmail.com>
2025-06-08 11:19:51 +0100

    fix first view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div>
-      <h2 className="font-semibold text-red-500">
+      <h2 className="font-semibold text-red-600 bg-blue-100 text-center">
         Hello from React with Tailwind
       </h2>
     </div>
